779c3a7da11a427cde00943c504b3a33b19badbc
cjswo198 <cjswo198@naver.com>
2016-08-29 22:41:32 +0900

    add lists

--- app.js
+++ app.js
@@ -2,91 +2,108 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-mongoose.connect("mongodb://localhost:27017")
+mongoose.connect("mongodb://localhost:50000/person");
 var db = mongoose.connection;
 db.once("open", function(){
   console.log("DB Connected!");
 });
 db.on("error", function(err){
   console.log("DB ERROR :", err);
-})
+});
 
 
 var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+  title: {type:String},
+  body: {type:String},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
 
-var Data = mongoose.model("data", dataSchema);
-
-
-Data.findOne({name:"myData"}, function(err, data){
-    if(err){
-      return console.log("Data Error!");
-    }
-
-    if(!data){
-      Data.create({name:"myData", count:0}, function(err, data){
-        if(err){
-          return console.log("Data create error!");
-        }
-
-        console.log("Success data initialization!", data);
-
-      });
-    }
-});
+var Post = mongoose.model("user", dataSchema);
 
 app.set("view engine", "ejs");
+
+//setting middlewares
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json()); //request body를 json형태로 바꿈.
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); //delete(request 신호)를 보안상 막기 때문....
 
 
-app.get('/', function(req, res){
-  getCounter(res);
-});
-
-app.get('/reset', function(req, res){
-  setCounter(res, 0);
-});
-
-app.get("/set/count", function(req, res){
-  if(req.query.count){
-    setCounter(res, req.query.count);
-  }
-});
-
-app.get("/set/:num", function (req, res){
-  setCounter(res, req.params.num);
-});
-
-function getCounter(res){
-  Data.findOne({name:"myData"}, function(err, data){
+//setting routes
+app.get('/posts', function(req, res){
+  Post.find({}).sort('-createdAt').exec(function(err, posts){
     if(err){
-      return console.log("DATA ERROR! : "+err);
+      return res.json({success:false, message:err});
     }
 
-    res.render("my_first_ejs", data);
+    res.render("posts/index", {data:posts});
   });
-}
+}); //index(RESTful)
 
-function setCounter(res, tempCount){
-  Data.findOne({name:"myData"}, function(err, data){
+app.get('/posts/new', function(req, res){
+  res.render("posts/new");
+}); //new(RESTful)
+
+
+app.post('/posts', function(req, res){
+  Post.create(req.body.post, function(err, post){
     if(err){
-      return console.log("DATA ERROR! : "+err);
+      return res.json({success:false, message:err});
     }
 
-    data.count = tempCount;
-    Data.update({name:"myData"}, {count:data.count}, function(err){
-      if(err){
-        return console.log("DATA ERROR! : "+err);
-      }
-
-      res.render("my_first_ejs", data);
-    });
-
+    res.redirect('/posts'); //처리 후 index 페이지로 이동
   });
-}
+});//create(RESTful)
+
+
+
+app.get('/posts/:id', function(req, res){
+  Post.findById({_id:req.params.id}, function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+
+    return res.render("posts/show", {data:post});
+  });
+});//show(RESTful)
+
+app.delete('/posts/:id', function(req, res){
+  Post.findByIdAndRemove({_id:req.params.id}, function(err, post){
+    if(err){
+      res.json({success:false, message:err});
+    }
+
+    res.redirect("/posts"); //처리후 index페이지로 이동.
+  });
+});//delete(RESTful)
+
+app.get('/posts/:id/edit', function(req, res){
+  Post.findById({_id:req.params.id}, function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+
+    return res.render("posts/edit", {data:post});
+  });
+
+});
+
+app.put('/posts/:id', function(req, res){
+  req.body.post.updatedAt = Date.now();
+  Post.findByIdAndUpdate({_id:req.params.id}, req.body.post , function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+
+    return res.redirect('/posts/'+req.params.id); //file path가 아니라 request path
+  });
+
+});
+
 
 app.listen(3000, function(){
   console.log('Server On!');
